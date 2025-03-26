3f3351d1b552a5a00c0e5ef890e7c41cbd09ffd9
CakesnCodes <scribblechronicles@protonmail.com>
2025-03-26 10:07:30 +0400

    Add files via upload

---
+++ Chapter 5/C5 bonus/soundboard.js
@@ -0,0 +1,47 @@
+var audio1 = document.getElementById("ah-ha");
+
+var audio2 = document.getElementById("BOTN");
+
+var audio3 = document.getElementById("BOOO");
+
+var audio4 = document.getElementById("dan");
+
+var audio5 = document.getElementById("EOTE");
+
+var audio6 = document.getElementById("hello");
+
+var audio7 = document.getElementById("scotchegg");
+
+var audio8 = document.getElementById("confusion");
+
+
+function play1() {
+  audio1.play();
+}
+function play2() {
+  audio2.play();
+}
+
+function play3() {
+  audio3.play();
+}
+
+function play4() {
+  audio4.play();
+}
+
+function play5() {
+  audio5.play();
+}
+
+function play6() {
+  audio6.play();
+}
+
+function play7() {
+  audio7.play();
+}
+
+function play8() {
+  audio8.play();
+}
